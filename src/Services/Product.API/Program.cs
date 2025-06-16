a8329bd26764d995356b9633cc3a5160716f8620
Nguyễn Việt <vietbmt19@gmail.com>
2025-06-17 02:24:01 +0700

    add common-logging for all project

--- src/Services/Product.API/Program.cs
+++ src/Services/Product.API/Program.cs
@@ -1,25 +1,50 @@
+﻿using Common.Logging;
+using Serilog;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure Serilog for logging
+builder.Host.UseSerilog(Serilogger.Configure);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Information("Starting Product API up");
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    // Use serilog cho full project
+    //ctx host builder context
+    //lc: logger configuration
+    //builder.Host.UseSerilog((ctx, lc) => lc
+    //    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+    //    .Enrich.FromLogContext()
+    //    .ReadFrom.Configuration(ctx.Configuration)
+    //);
+
+    // Add services
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application failed to start correctly");
+}
+finally
+{
+    Log.Information("Shut down Product API complete");
+    Log.CloseAndFlush(); // ghi lai log
+}
