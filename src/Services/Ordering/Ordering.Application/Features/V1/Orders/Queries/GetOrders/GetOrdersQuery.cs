250990aa0df7984cbe2a1f8d9dc32961f9697a34
Vietokeman <vietbmt19@gmail.com>
2025-11-28 19:06:21 +0700

    chore: implement get order by username

--- src/Services/Ordering/Ordering.Application/Features/V1/Orders/Queries/GetOrders/GetOrdersQuery.cs
+++ src/Services/Ordering/Ordering.Application/Features/V1/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MediatR;
+using Ordering.Application.Common.Models;
+using Shared.SeedWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,12 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.V1.Orders.Queries.GetOrders
 {
-    public class GetOrdersQuery
+    public class GetOrdersQuery : IRequest<ApiResult<List<OrderDto>>>
     {
+        public string UserName { get; private set; }
+        public GetOrdersQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
     }
 }
--- src/Services/Ordering/Ordering.Application/Features/V1/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/V1/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Application.Common.Models;
+using Ordering.Application.Features.V1.Orders.Queries.GetOrders;
+using Shared.SeedWork;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +12,20 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.V1.Orders
 {
-    public class GetOrdersQueryHandler : IRequest<>
+    public class GetOrdersQueryHandler : IRequestHandler<GetOrdersQuery, ApiResult<List<OrderDto>>>
     {
+        private readonly IMapper _mapper;
+        private readonly IOrderRepository _orderRepository;
+        public GetOrdersQueryHandler(IMapper mapper,IOrderRepository orderRepository)
+        {
+             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+             _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        }
+        public async Task<ApiResult<List<OrderDto>>> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var orderEntitites = await _orderRepository.GetOrderByUserName(request.UserName);
+            var orderList = _mapper.Map<List<OrderDto>>(orderEntitites);
+            return new ApiSuccessResult<List<OrderDto>>(orderList, "Get orders by user name successfully");
+        }
     }
 }
