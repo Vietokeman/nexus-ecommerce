fd7d5cd7ce4774a152f6bb0bc3af0af5b6230d1b
Vietokeman <vietbmt19@gmail.com>
2026-03-22 02:05:03 +0700

    feat(react-ui): add explicit filter open/close actions

--- src/WebApps/React.Client/src/store/ui-store.ts
+++ src/WebApps/React.Client/src/store/ui-store.ts
@@ -5,6 +5,8 @@ interface UIState {
   isFilterOpen: boolean;
   isMobileMenuOpen: boolean;
   toggleFilter: () => void;
+  openFilter: () => void;
+  closeFilter: () => void;
   toggleMobileMenu: () => void;
   closeAll: () => void;
 }
@@ -15,6 +17,8 @@ export const useUIStore = create<UIState>()(
       isFilterOpen: false,
       isMobileMenuOpen: false,
       toggleFilter: () => set((s) => ({ isFilterOpen: !s.isFilterOpen })),
+      openFilter: () => set({ isFilterOpen: true }),
+      closeFilter: () => set({ isFilterOpen: false }),
       toggleMobileMenu: () => set((s) => ({ isMobileMenuOpen: !s.isMobileMenuOpen })),
       closeAll: () => set({ isFilterOpen: false, isMobileMenuOpen: false }),
     }),
